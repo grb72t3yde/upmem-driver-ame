fcef25419804e19f028d9ba1a38d7e286114a6b9
Yun-Ze Li <p76091292@gs.ncku.edu.tw>
2022-07-04 15:56:12 +0000

    ame: introduce ame_context_t
    strcut ame_context is per-node meta data used by AME.
    
    Signed-off-by: Yun-Ze Li <p76091292@gs.ncku.edu.tw>

--- modules/dpu_ame.c
+++ modules/dpu_ame.c
@@ -1,1 +1,3 @@
 #include "dpu_ame.h"
+
+ame_context_t *ame_context_list[MAX_NUMNODES];
--- modules/dpu_ame.h
+++ modules/dpu_ame.h
@@ -1,3 +1,16 @@
 #ifndef DPU_AME_H
 #define DPU_AME_H
+
+#include <linux/list.h>
+#include <dpu_region.h>
+
+typedef struct ame_context {
+    int nid;
+    struct list_head rank_list;
+    struct list_head ltb_rank_list;
+    struct dpu_rank_t *ltb_index;
+    atomic_t nr_free_ranks;
+    atomic_t nr_ltb_ranks;
+} ame_context_t;
+
 #endif
