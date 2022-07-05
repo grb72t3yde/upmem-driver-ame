4610f755370eeae5b0666b4270a999d95f3ff331
Yun-Ze Li <p76091292@gs.ncku.edu.tw>
2022-07-05 02:46:32 +0000

    ame: introduce init_ame_context and ame_init
    init_ame_context initializes sturct ame_context fields for a node.
    
    Signed-off-by: Yun-Ze Li <p76091292@gs.ncku.edu.tw>

--- modules/dpu_ame.c
+++ modules/dpu_ame.c
@@ -1,3 +1,40 @@
+#include <linux/nodemask.h>
+
 #include "dpu_ame.h"
 
 ame_context_t *ame_context_list[MAX_NUMNODES];
+
+static int init_ame_context(int nid)
+{
+    ame_context_list[nid] = kzalloc(sizeof(ame_context_t), GFP_KERNEL);
+
+    if (!ame_context_list[nid])
+        return -ENOMEM;
+
+    INIT_LIST_HEAD(&ame_context_list[nid]->rank_list);
+    INIT_LIST_HEAD(&ame_context_list[nid]->ltb_rank_list);
+
+    ame_context_list[nid]->ltb_index = NULL;
+    ame_context_list[nid]->nid = nid;
+    atomic_set(&ame_context_list[nid]->nr_free_ranks, 0);
+    atomic_set(&ame_context_list[nid]->nr_ltb_ranks, 0);
+
+    return 0;
+}
+
+int ame_init(void)
+{
+    int node;
+
+    for_each_online_node(node)
+        if (init_ame_context(node))
+            goto out_no_mem;
+
+    return 0;
+
+out_no_mem:
+    for_each_online_node(node)
+        if (ame_context_list[node])
+            kfree(ame_context_list[node]);
+    return -ENOMEM;
+}
